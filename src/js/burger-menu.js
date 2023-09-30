1cbcee2587e321cfeb9b62e8f39fe4c0c8fd703c
mr-dorian-black <mr-dorian-black@users.noreply.github.com>
2023-09-30 23:36:40 +0300

    mobile version v1.14

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -4,6 +4,7 @@ let menuList = document.querySelector('.burger-menu-list');
 let logo = document.querySelector('.header-svg-logo');
 
 let htmlTag = document.querySelector('html');
+let bodyTag = document.querySelector('body');
 
 openBtn.forEach(btn => btn.addEventListener('click', toggleMenu));
 menuList.addEventListener('click', e => {
@@ -14,13 +15,17 @@ function toggleMenu() {
      menu.classList.toggle('is-open');
      if (menu.classList.contains('is-open')) {
           htmlTag.style.overflow = 'hidden';
-          // document.body.classList.add('menu-open');
-          // menu.style.overflow = 'hidden';
+          bodyTag.style.overflow = 'hidden';
+          // bodyTag.classList.add('menu-open');
+          menu.style.overflow = 'hidden';
+          // disableScroll();
      }
      else {
           htmlTag.style.overflow = 'auto';
-          // document.body.classList.remove('menu-open');
-          // menu.style.overflow = 'auto';
+          bodyTag.style.overflow = 'auto';
+          // bodyTag.classList.remove('menu-open');
+          menu.style.overflow = 'auto';
+          // enableScroll();
      }
 };
 
@@ -29,3 +34,26 @@ window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
      if (!e.matches) return;
      menu.classList.remove('is-open');
 });
+
+
+// Заблокировать скроллинг страницы
+function disableScroll() {
+     // Получить текущее положение прокрутки страницы
+     const scrollY = window.scrollY;
+     // Сохранить текущее положение прокрутки в стиле страницы
+     document.body.style.position = 'fixed';
+     document.body.style.top = `-${scrollY}px`;
+}
+
+// Разблокировать скроллинг страницы
+function enableScroll() {
+     const scrollY = parseInt(document.body.style.top || '0');
+     // Удалить стили блокировки прокрутки
+     document.body.style.position = '';
+     document.body.style.top = '';
+     // Восстановить положение прокрутки
+     window.scrollTo(0, scrollY);
+}
+
+// Для блокировки скроллинга вызовите функцию disableScroll()
+// Для разблокировки скроллинга вызовите функцию enableScroll()
