bda9d502291de1346ca8c2a7f3f85f1a8d4d805b
Dorian <d.black.personal@gmail.com>
2023-10-22 16:57:06 +0300

    v4.0

--- src/js/payments.js
+++ src/js/payments.js
@@ -5,37 +5,37 @@ import 'notyf/notyf.min.css';
 const payments = [
      {
           type: 'UAH', name: "UAH Перекази по Україні", details: [
-               { info: `ГО ‘БЛЕКАУТ’</br>ЄДРПОУ 44687081</br>Призначення платежу:</br>"Благодійний внесок на статутну діяльність"</br>Назва банку: АТ КБ "ПРИВАТБАНК"</br>UA383052990000026000045028181`, copy: "UA383052990000026000045028181" },
+               { info: `<span class="text-bold">ГО ‘БЛЕКАУТ’</span></br>ЄДРПОУ 44687081</br>Призначення платежу:</br>"Благодійний внесок на статутну діяльність"</br>Назва банку: АТ КБ "ПРИВАТБАНК"</br>UA383052990000026000045028181`, copy: "UA383052990000026000045028181" },
 
           ]
      },
      {
           type: "SEPA", name: "SEPA in euros (EUR)", details: [
                {
-                    info: `Bank: Clear Junction Limited</br>IBAN: G825CLJU00997183555952</br>BIC: CLJUGB21</br>Account Holder Name: NEDILKO ANDRII</br>Taxpayer Identification Number: 3522304691</br>Bank Address: 5 Kingsway, London WC2B 6UN`, copy: "G825CLJU00997183555952"
+                    info: `<span class="text-bold">Bank: Clear Junction Limited</span></br>IBAN: G825CLJU00997183555952</br>BIC: CLJUGB21</br>Account Holder Name: NEDILKO ANDRII</br>Taxpayer Identification Number: 3522304691</br>Bank Address: 5 Kingsway, London WC2B 6UN`, copy: "G825CLJU00997183555952"
                }
           ]
      },
      {
           type: "SWIFT", name: "Валютні SWIFT-перекази", details: [
                {
-                    info: `SWIFT in US dollars (USD)</br>BENEFICIARY FRONTLINE.CARE</br>IBAN: UA673052990000026002045026136`, copy: "UA673052990000026002045026136"
+                    info: `<span class="text-bold">SWIFT in US dollars (USD)</span></br>BENEFICIARY FRONTLINE.CARE</br>IBAN: UA673052990000026002045026136`, copy: "UA673052990000026002045026136"
                },
                {
-                    info: `SWIFT in euros (EUR)</br>BENEFICIARY FRONTLINE.CARE</br>IBAN: UA713052990000026004035028012`, copy: "UA713052990000026004035028012"
+                    info: `<span class="text-bold">SWIFT in euros (EUR)</span></br>BENEFICIARY FRONTLINE.CARE</br>IBAN: UA713052990000026004035028012`, copy: "UA713052990000026004035028012"
                }
           ]
      },
      {
           type: "Crypto", name: "Криптовалюта", details: [
                {
-                    info: `Bitcoin:</br>159cWtQWWEmSZXbwZeL.pxuyuJMZAtZEWR8`, copy: "159cWtQWWEmSZXbwZeL.pxuyuJMZAtZEWR8"
+                    info: `<span class="text-bold">Bitcoin:</span></br>159cWtQWWEmSZXbwZeL.pxuyuJMZAtZEWR8`, copy: "159cWtQWWEmSZXbwZeL.pxuyuJMZAtZEWR8"
                },
                {
-                    info: `USDT (TRC20):</br>TSIZNXJc3UifS5ErXeAmgGrpo32grv2n3F`, copy: "TSIZNXJc3UifS5ErXeAmgGrpo32grv2n3F"
+                    info: `<span class="text-bold">USDT (TRC20):</span></br>TSIZNXJc3UifS5ErXeAmgGrpo32grv2n3F`, copy: "TSIZNXJc3UifS5ErXeAmgGrpo32grv2n3F"
                },
                {
-                    info: `ETH (ERC20):</br>0x2bd19782e5b0dda4469c87316080719cffc73d11`, copy: "0x2bd19782e5b0dda4469c87316080719cffc73d11"
+                    info: `<span class="text-bold">ETH (ERC20):</span></br>0x2bd19782e5b0dda4469c87316080719cffc73d11`, copy: "0x2bd19782e5b0dda4469c87316080719cffc73d11"
                }
           ]
      },
